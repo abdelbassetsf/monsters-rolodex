11bda2d09e9be8a14af58710ab9d5706fdad9d9d
Abdelbasset Bouremani <abdelbassetsf@gmail.com>
2020-09-29 00:04:45 +0100

    Updates

---
+++ precache-manifest.f3e78c432c2ab1581841f965dec48a44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03a2b5fd27bed2b01b59fe9f981346af",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ea91075c53d13344312c",
+    "url": "/monsters-rolodex/static/css/main.3b869e84.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea91075c53d13344312c",
+    "url": "/monsters-rolodex/static/js/main.c42abd5c.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
